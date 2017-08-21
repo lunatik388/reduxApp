a2c16c58819f04653df962bcdab01e8246641742
formago <formago.max@gmail.com>
2017-08-22 01:03:07 +0300

    конец 1 части

--- public/bundle.js
+++ public/bundle.js
@@ -701,64 +701,50 @@ function compose() {
 
 var _redux = __webpack_require__(8);
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+var _reduxLogger = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"redux-logger\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
-//STEP 3 define reducers
-var reducer = function reducer() {
-    var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { books: [] };
-    var action = arguments[1];
+var _reduxLogger2 = _interopRequireDefault(_reduxLogger);
 
-    switch (action.type) {
-        case "POST_BOOK":
-            // let books =  state.books.concat(action.payload);
-            // return {books};
-            return {
-                books: [].concat(_toConsumableArray(state.books), _toConsumableArray(action.payload))
-            };
-            break;
-        case "DELETE_BOOK":
-            // Create a copy of the current array of books
-            var currentBookToDelete = [].concat(_toConsumableArray(state.books));
-            // Determine at which index in books  array is the book to be deleted
-            var indexToDelete = currentBookToDelete.findIndex(function (book) {
-                return book.id === action.payload.id;
-            });
-            //use slice to remove the book at the  specified index
-            return {
-                books: [].concat(_toConsumableArray(currentBookToDelete.slice(0, indexToDelete)), _toConsumableArray(currentBookToDelete.slice(indexToDelete + 1)))
-            };
-            break;
-    }
-    return state;
-};
+var _index = __webpack_require__(24);
 
-// STEP 1 create the storec
-var store = (0, _redux.createStore)(reducer);
+var _index2 = _interopRequireDefault(_index);
+
+var _cartActions = __webpack_require__(27);
+
+var _booksActions = __webpack_require__(28);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// STEP 1 create the store
+var middleware = (0, _redux.applyMiddleware)(_reduxLogger2.default);
+var store = (0, _redux.createStore)(_index2.default, middleware);
 
 store.subscribe(function () {
     console.log('current state is: ', store.getState());
     //console.log('current price: ', store.getState()[1].price);
 });
-// STEP 2 create and dispatch actions
-store.dispatch({
-    type: "POST_BOOK",
-    payload: [{
-        id: 1,
-        title: 'this is the book title',
-        description: 'this is the book description',
-        price: 33.33
-    }, {
-        id: 2,
-        title: 'this is the second book title',
-        description: 'this is the second book description',
-        price: 50
-    }]
-});
+store.dispatch((0, _booksActions.postBooks)([{
+    id: 1,
+    title: 'this is the book title',
+    description: 'this is the book description',
+    price: 33.33
+}, {
+    id: 2,
+    title: 'this is the second book title',
+    description: 'this is the second book  description',
+    price: 50
+}]));
 // DELETE a book
-store.dispatch({
-    type: "DELETE_BOOK",
-    payload: { id: 1 }
-});
+store.dispatch((0, _booksActions.deleteBooks)({ id: 1 }));
+// UPDATE a book
+store.dispatch((0, _booksActions.updateBooks)({
+    id: 2,
+    title: 'Learn React in 24h'
+
+}));
+
+// UPDATE a book
+store.dispatch((0, _cartActions.addToCart)([{ id: 1 }]));
 
 /***/ }),
 /* 8 */
@@ -1368,6 +1354,177 @@ function applyMiddleware() {
       });
     };
   };
+}
+
+/***/ }),
+/* 24 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _redux = __webpack_require__(8);
+
+var _booksReducers = __webpack_require__(25);
+
+var _cartReducers = __webpack_require__(26);
+
+exports.default = (0, _redux.combineReducers)({
+    books: _booksReducers.booksReducers,
+    cart: _cartReducers.cartReducers
+});
+
+/***/ }),
+/* 25 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+//STEP 3 define reducers
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.booksReducers = booksReducers;
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function booksReducers() {
+    var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { books: [] };
+    var action = arguments[1];
+
+    switch (action.type) {
+        case "POST_BOOK":
+            // let books =  state.books.concat(action.payload);
+            // return {books};
+            return {
+                books: [].concat(_toConsumableArray(state.books), _toConsumableArray(action.payload))
+            };
+            break;
+        case "DELETE_BOOK":
+            // Create a copy of the current array of books
+            var currentBookToDelete = [].concat(_toConsumableArray(state.books));
+            // Determine at which index in books  array is the book to be deleted
+            var indexToDelete = currentBookToDelete.findIndex(function (book) {
+                return book.id === action.payload.id;
+            });
+            //use slice to remove the book at the  specified index
+            return {
+                books: [].concat(_toConsumableArray(currentBookToDelete.slice(0, indexToDelete)), _toConsumableArray(currentBookToDelete.slice(indexToDelete + 1)))
+            };
+            break;
+        case "UPDATE_BOOK":
+
+            // Create a copy of the current array of  books
+            var currentBookToUpdate = [].concat(_toConsumableArray(state.books));
+            // Determine at which index in books      array is the book to be deleted
+            var indexToUpdate = currentBookToUpdate.findIndex(function (book) {
+                return book.id === action.payload.id;
+            });
+            // Create a new book object with the new values and with the same array index of the item we want to replace. To achieve this we
+            //will use ...spread but we could use concat methos too
+            var newBookToUpdate = _extends({}, currentBookToUpdate[indexToUpdate], {
+                title: action.payload.title
+                // This Log has the purpose to show you   how newBookToUpdate looks like
+            });console.log("what is it newBookToUpdate", newBookToUpdate);
+            //use slice to remove the book at the      specified index, replace with the new object
+            //and concatenate witht he rest of items in the array
+            return {
+                books: [].concat(_toConsumableArray(currentBookToUpdate.slice(0, indexToUpdate)), [newBookToUpdate], _toConsumableArray(currentBookToUpdate.slice(indexToUpdate + 1)))
+            };
+            break;
+    }
+    return state;
+}
+
+/***/ }),
+/* 26 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+"user strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.cartReducers = cartReducers;
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function cartReducers() {
+    var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { cart: [] };
+    var action = arguments[1];
+
+    switch (action.type) {
+        case "ADD_TO_CART":
+            return { cart: [].concat(_toConsumableArray(state.cart), _toConsumableArray(action.payload)) };
+            break;
+    }
+    return state;
+}
+
+/***/ }),
+/* 27 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// ADD TO CART
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.addToCart = addToCart;
+function addToCart(book) {
+    return {
+        type: "ADD_TO_CART",
+        payload: book
+    };
+}
+
+/***/ }),
+/* 28 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+// POST A BOOK
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.postBooks = postBooks;
+exports.deleteBooks = deleteBooks;
+exports.updateBooks = updateBooks;
+function postBooks(book) {
+    return {
+        type: "POST_BOOK",
+        payload: book
+    };
+}
+// DELETE A BOOK
+function deleteBooks(id) {
+    return {
+        type: "DELETE_BOOK",
+        payload: id
+    };
+}
+// UPDATE A BOOK
+function updateBooks(book) {
+    return {
+        type: "UPDATE_BOOK",
+        payload: book
+    };
 }
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,47 +1,22 @@
 "use strict"
-import { createStore } from 'redux';
-//STEP 3 define reducers
-const reducer = function (state = { books: [] },
-    action) {
-    switch (action.type) {
-        case "POST_BOOK":
-            // let books =  state.books.concat(action.payload);
-            // return {books};
-            return {
-                books: [...state.books, ...action.payload]
-            }
-            break;
-        case "DELETE_BOOK":
-            // Create a copy of the current array of books
-            const currentBookToDelete = [...state.books]
-            // Determine at which index in books  array is the book to be deleted
-            const indexToDelete =
-                currentBookToDelete.findIndex(
-                    function (book) {
-                        return book.id === action.payload.id;
-                    }
-                )
-            //use slice to remove the book at the  specified index
-            return {
-                books: [...currentBookToDelete.slice(0,indexToDelete),
-                     ...currentBookToDelete.slice(indexToDelete +  1)]
-            }
-            break;
-    }
-    return state
-}
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import reducers from './reducers/index';
+import { addToCart } from "./actions/cartActions";
+import { postBooks, deleteBooks, updateBooks } from './actions/booksActions';
 
-// STEP 1 create the storec
-const store = createStore(reducer);
+// STEP 1 create the store
+const middleware = applyMiddleware(logger);
+const store = createStore(reducers, middleware);
+
+
 
 store.subscribe(function () {
     console.log('current state is: ', store.getState());
     //console.log('current price: ', store.getState()[1].price);
 })
-// STEP 2 create and dispatch actions
-store.dispatch({
-    type: "POST_BOOK",
-    payload: [{
+store.dispatch(postBooks(
+    [{
         id: 1,
         title: 'this is the book title',
         description: 'this is the book description',
@@ -50,12 +25,22 @@ store.dispatch({
     {
         id: 2,
         title: 'this is the second book title',
-        description: 'this is the second book description',
+        description: 'this is the second book  description',
         price: 50
     }]
-})
+))
 // DELETE a book
-store.dispatch({
-    type: "DELETE_BOOK",
-    payload: { id: 1 }
-})
+store.dispatch(deleteBooks(
+    { id: 1 }
+))
+// UPDATE a book
+store.dispatch(updateBooks(
+    {
+        id: 2,
+        title: 'Learn React in 24h'
+
+    }
+))
+
+// UPDATE a book
+store.dispatch(addToCart([{ id: 1 }]))
